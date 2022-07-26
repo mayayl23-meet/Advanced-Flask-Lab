83d531616b89cd2e6448f6550a3e7b892d309593
mayayl23-meet <109899357+mayayl23-meet@users.noreply.github.com>
2022-07-26 16:45:55 +0300

    Advanced Flask Lab - Completed

--- Advanced-Flask-Lab/app.py
+++ Advanced-Flask-Lab/app.py
@@ -9,9 +9,9 @@ app = Flask(  # Create a flask app
 )
 
 # Variables for tasks
-image_link = "https://scontent.ftlv21-1.fna.fbcdn.net/v/t31.18172-1/1502644_10152506946962507_2442851309927378964_o.png?stp=c49.0.148.148a_dst-png_p148x148&_nc_cat=111&ccb=1-7&_nc_sid=1eb0c7&_nc_ohc=d-R_wjIm2d8AX_NugPv&_nc_ht=scontent.ftlv21-1.fna&oh=00_AT8CSjiG8-biys_qQ9tU-yrxzJSp6HANxsz5c3mFVk-G2Q&oe=62FD0A1B"
+image_link = "static/images/coffee.jpeg"
 
-user_bio = "Middle East Entrepreneurs of Tomorrow. Enabling the next generation of Israeli and Palestinian leaders."
+user_bio = "Coffee House: The Best Coffee In The World!"
 
 posts = {
     "https://scontent.ftlv1-1.fna.fbcdn.net/v/t39.30808-6/243021542_10158462822407507_4186737446569136175_n.jpg?stp=cp0_dst-jpg_e15_fr_q65&_nc_cat=101&ccb=1-7&_nc_sid=ed5ff1&_nc_ohc=nHa7N_8RkP0AX92LvUb&_nc_ht=scontent.ftlv1-1.fna&oh=00_AT8kLSHR6V-bvFjpXVVzHXWXviYhBJgkgHANsGlmTojibg&oe=62E08013": "2021 cohort's Y3 Accelerator!",
@@ -25,8 +25,7 @@ posts = {
 
 @app.route('/')  # '/' for the default page
 def home():
-    return render_template('index.html')
-
+    return render_template('index.html', image_link=image_link, user_bio=user_bio, posts=posts)
 
 @app.route('/about')  # '/' for the default page
 def about():
